0a5b7c9ba4915fd55d163329857b8ed397301e59
WizeRacoon <mika.sipman@gmail.com>
2025-05-27 17:39:20 +0200

    small change to import atelectasis

--- atelectasis.py
+++ atelectasis.py
@@ -1,12 +1,12 @@
 # This is the main file of the project
 
-import data_processor as dp, new_PSO as nPSO, post_PSO as pPSO, differential_function as df, kaggleprep as kp
+import data_processor as dp, new_PSO as nPSO, post_PSO as pPSO, differential_function as df
 import os
 from math import floor
 
 #================================================================================================
 ### Experiment name------------------
-experiment_name                     = "Experiment-3"
+experiment_name                     = "Experiment"
 
 
 ### Input data location--------------
@@ -60,36 +60,35 @@ def main():
     print(f"Created {len(image_data_objects)} image data objects.")
     
     for image_data_object in image_data_objects:
-        if image_data_object.view_position == "PA":
-            image_name = image_data_object.image_index.split(".")[0]
+        image_name = image_data_object.image_index.split(".")[0]
 
-            if apply_PSO:
-                nPSO.PSO(
-                    n=n, 
-                    N=N, 
-                    image_path= f"{images_directory}/{image_data_object.image_index}", 
-                    max_iterations = max_iterations,
-                    output_directory = PSO_output_directory)
-            
-            #pso_images = os.listdir(f"{PSO_output_directory}/{img_name}")
-            
-            if apply_masking:
-                pPSO.lung_mask(
-                    image_name = image_name,
-                    PSO_image_relative_path = f"{PSO_output_directory}/{image_name}/lung_position_clip/velocity_clip/lung_reconstructed_palette_iter_{max_iterations-1}.png", 
-                    output_directory = masking_output_directory,
-                    save_intermediate = save_intermediate,
-                    intermediate_directory = masking_intermediate_directory)
-            
-            if apply_differential_function: 
-                image_data_object.symmetry_percentage, image_data_object.proportional_lung_capacity = df.calc_lung_symmetry(
-                    image_index = image_data_object.image_index,
-                    masked_lung_image_relative_path = f"{masking_output_directory}/{image_data_object.image_index}",
-                    show_symmetry_line = show_symmetry_line,
-                    save_symmetry_line = save_symmetry_line, 
-                    symmetry_output_directory = symmetry_output_directory, 
-                    print_findings = print_findings
-                    )
+        if apply_PSO:
+            nPSO.PSO(
+                n=n, 
+                N=N, 
+                image_path= f"{images_directory}/{image_data_object.image_index}", 
+                max_iterations = max_iterations,
+                output_directory = PSO_output_directory)
+        
+        #pso_images = os.listdir(f"{PSO_output_directory}/{img_name}")
+        
+        if apply_masking:
+            pPSO.lung_mask(
+                image_name = image_name,
+                PSO_image_relative_path = f"{PSO_output_directory}/{image_name}/lung_position_clip/velocity_clip/lung_reconstructed_palette_iter_{max_iterations-1}.png", 
+                output_directory = masking_output_directory,
+                save_intermediate = save_intermediate,
+                intermediate_directory = masking_intermediate_directory)
+        
+        if apply_differential_function: 
+            image_data_object.symmetry_percentage, image_data_object.proportional_lung_capacity = df.calc_lung_symmetry(
+                image_index = image_data_object.image_index,
+                masked_lung_image_relative_path = f"{masking_output_directory}/{image_data_object.image_index}",
+                show_symmetry_line = show_symmetry_line,
+                save_symmetry_line = save_symmetry_line, 
+                symmetry_output_directory = symmetry_output_directory, 
+                print_findings = print_findings
+                )
 
 if __name__ == "__main__":
     main()  # call the main function when the script is run directly
